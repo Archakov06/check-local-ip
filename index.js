e22299e541f4deddf4f177e949365473e49bc158
Archakov Dennis <hello@archakov.im>
2017-11-30 14:52:26 +0300

    fix ports

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const request = require('request');
 
-const check = (ip, port = 3000) =>
-  request
-    .head(`http://${ip}:${port}/`, { timeout: 500 })
-    .on('response', function(response) {
-      console.log(`http://${ip}:${port}/ - OK`);
-    })
-    .on('error', function(response) {});
+const check = (ip, port = [3000]) => {
+  for (var i = 0; i < port.length; i++) {
+    const adress = `http://${ip}:${port[i]}/`;
+    request
+      .head(adress, { timeout: 500 })
+      .on('response', response => console.log(`${adress} - OK`))
+      .on('error', () => {});
+  }
+};
 
 for (var i = 0; i < 2; i++) {
   for (var o = 0; o < 120; o++) {
-    check(`192.168.${i}.${o}`);
+    check(`192.168.${i}.${o}`, [3000, 8080, 80]);
   }
 }
